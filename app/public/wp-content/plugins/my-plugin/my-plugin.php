8498c0ccf45edd0f6714ebeb78fae5e5d6844f9e
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2020-11-22 18:43:57 +0900

    wordpressのブロックエディターにJavaScriptを読み込ませた

---
+++ app/public/wp-content/plugins/my-plugin/my-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ Plugin Name: マイプラグイン
+ Author: takaki
+*/
+
+add_action('enqueue_block_editor_assets', function () {
+  wp_enqueue_script(
+    'myeditor-sctipt',
+    plugins_url('myeditor.js', __FILE__)
+  );
+});
